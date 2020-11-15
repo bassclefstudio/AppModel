1f12566a4fe6a38f7eb64a99d93df1ef58ed7ac2
bassclefstudio <bassclefstudio-dev@outlook.com>
2020-11-14 22:28:32 -0500

    Added file system services.

---
+++ BassClefStudio.Mvvm.Services/Files/FileExceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BassClefStudio.Mvvm.Services.Files
+{
+    /// <summary>
+    /// An <see cref="Exception"/> thrown when a file, folder, or file content exists, but the current context does not have the permission to access it.
+    /// </summary>
+    [Serializable]
+    public class FilePermissionException : Exception
+    {
+        /// <inheritdoc/>
+        public FilePermissionException() { }
+        /// <inheritdoc/>
+        public FilePermissionException(string message) : base(message) { }
+        /// <inheritdoc/>
+        public FilePermissionException(string message, Exception inner) : base(message, inner) { }
+        /// <inheritdoc/>
+        protected FilePermissionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    /// <summary>
+    /// An <see cref="Exception"/> thrown when accessing or finding an item in the file system fails.
+    /// </summary>
+    [Serializable]
+    public class FileAccessException : Exception
+    {
+        /// <inheritdoc/>
+        public FileAccessException() { }
+        /// <inheritdoc/>
+        public FileAccessException(string message) : base(message) { }
+        /// <inheritdoc/>
+        public FileAccessException(string message, Exception inner) : base(message, inner) { }
+        /// <inheritdoc/>
+        protected FileAccessException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ BassClefStudio.Mvvm.Services/Files/IStorageItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BassClefStudio.Mvvm.Services.Files
+{
+    /// <summary>
+    /// Represents a file or directory on the filesystem of a platform.
+    /// </summary>
+    public interface IStorageItem
+    {
+        /// <summary>
+        /// The name of the <see cref="IStorageItem"/>, including any file extension.
+        /// </summary>
+        string Name { get; }
+    }
+}
