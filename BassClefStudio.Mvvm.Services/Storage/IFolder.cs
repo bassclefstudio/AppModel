9d0252d4034ae9d1c6cde187b9604ef51d091db7
bassclefstudio <bassclefstudio-dev@outlook.com>
2020-11-15 21:41:59 -0500

    Added new item creation options to the IFolder interface.

--- BassClefStudio.Mvvm.Services/Storage/IFolder.cs
+++ BassClefStudio.Mvvm.Services/Storage/IFolder.cs
@@ -22,5 +22,20 @@ namespace BassClefStudio.Mvvm.Services.Storage
         /// <param name="relativePath">The <see cref="string"/> path to the item, relative to this <see cref="IFolder"/>.</param>
         /// <returns>the child <see cref="IStorageItem"/> at the given path.</returns>
         Task<IStorageItem> GetItem(string relativePath);
+
+        /// <summary>
+        /// Creates a new <see cref="IFile"/> in the folder.
+        /// </summary>
+        /// <param name="name">The name of the new file, including its file extension.</param>
+        /// <param name="overwrite">A <see cref="bool"/> indicating whether the file contents should be overwritten if the file exists. If 'false', opens the existing <see cref="IFile"/>.</param>
+        /// <returns>The newly created file.</returns>
+        Task<IFile> CreateFile(string name, bool overwrite = false);
+
+        /// <summary>
+        /// Creates a new <see cref="IFolder"/> in the folder.
+        /// </summary>
+        /// <param name="name">The name of the new folder.</param>
+        /// <returns>The newly created folder.</returns>
+        Task<IFile> CreateFolder(string name);
     }
 }
