a68ef9aa5bf0a6113cc4d087a65c3bfe234c883a
bassclefstudio <bassclefstudio-dev@outlook.com>
2021-01-24 10:38:52 -0500

    Replaced old BindingExpression with new IBinding interfaces.

---
+++ BassClefStudio.AppModel/Bindings/IBinding.cs
@@ -0,0 +1,110 @@
+﻿using BassClefStudio.NET.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BassClefStudio.AppModel.Bindings
+{
+    /// <summary>
+    /// Represents a reference to some <typeparamref name="T"/> value, with dynamic value-change notifications and direct get/set accessors.
+    /// </summary>
+    /// <typeparam name="T">The type of the object that this <see cref="IBinding{T}"/> represents.</typeparam>
+    public interface IBinding<T> : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Get or set the currently stored <typeparamref name="T"/> value this <see cref="IBinding{T}"/> represents.
+        /// </summary>
+        T StoredValue { get; set; }
+
+        /// <summary>
+        /// An event fired whenever the value of <see cref="StoredValue"/> changes. Equivalent to the <see cref="INotifyPropertyChanged"/> events from the <see cref="StoredValue"/> property.
+        /// </summary>
+        event EventHandler ValueChanged;
+    }
+
+    /// <summary>
+    /// Represents an abstract implementation of <see cref="IBinding{T}"/> that supports get-based, one-way data binding.
+    /// </summary>
+    /// <typeparam name="T">The type of the object that this <see cref="IBinding{T}"/> represents.</typeparam>
+    public abstract class OneWayBinding<T> : Observable, IBinding<T>
+    {
+        private T storedValue;
+
+        /// <inheritdoc/>
+        public T StoredValue 
+        { 
+            get => storedValue; 
+            set => throw new BindingException("OneWayBinding<T> does not support setting of the bound value."); 
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler ValueChanged;
+
+        /// <summary>
+        /// Gets the current value to update this <see cref="IBinding{T}"/>'s <see cref="StoredValue"/>.
+        /// </summary>
+        /// <returns></returns>
+        public abstract T GetValue();
+
+        /// <summary>
+        /// Calling this method causes the <see cref="OneWayBinding{T}"/> to update itself, triggering <see cref="ValueChanged"/> and similar events.
+        /// </summary>
+        public void UpdateBinding()
+        {
+            var newVal = GetValue();
+            if(newVal == null || !newVal.Equals(StoredValue))
+            {
+                Set(ref storedValue, newVal, nameof(StoredValue));
+                ValueChanged?.Invoke(this, new EventArgs());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents an abstract implementation of <see cref="IBinding{T}"/> that supports get- and set-based, two-way data binding.
+    /// </summary>
+    /// <typeparam name="T">The type of the object that this <see cref="IBinding{T}"/> represents.</typeparam>
+    public abstract class TwoWayBinding<T> : Observable, IBinding<T>
+    {
+        private T storedValue;
+
+        /// <inheritdoc/>
+        public T StoredValue
+        {
+            get => storedValue;
+            set 
+            {
+                SetValue(value);
+                UpdateBinding();
+            }
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler ValueChanged;
+
+        /// <summary>
+        /// Gets the current value to update this <see cref="IBinding{T}"/>'s <see cref="StoredValue"/>.
+        /// </summary>
+        public abstract T GetValue();
+
+        /// <summary>
+        /// Sets the backing store for this <see cref="TwoWayBinding{T}"/> to a given <typeparamref name="T"/> value.
+        /// </summary>
+        /// <param name="newVal">The <typeparamref name="T"/> value to set.</param>
+        public abstract void SetValue(T newVal);
+
+        /// <summary>
+        /// Calling this method causes the <see cref="TwoWayBinding{T}"/> to update itself, triggering <see cref="ValueChanged"/> and similar events.
+        /// </summary>
+        public void UpdateBinding()
+        {
+            var newVal = GetValue();
+            if (newVal == null || !newVal.Equals(StoredValue))
+            {
+                Set(ref storedValue, newVal, nameof(StoredValue));
+                ValueChanged?.Invoke(this, new EventArgs());
+            }
+        }
+    }
+}
